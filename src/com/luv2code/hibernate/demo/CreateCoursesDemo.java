1e0fbd21a5dc26872996553cd177f1f336dd5939
Terry Davis <davist15@email.franklin.edu>
2018-02-16 21:14:14 -0500

    Initial Commit

---
+++ src/com/luv2code/hibernate/demo/CreateCoursesDemo.java
@@ -0,0 +1,57 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class CreateCoursesDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .addAnnotatedClass(Course.class)
+								 .buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// start a transaction
+			session.beginTransaction();
+			
+			// get the instructor from db
+			int theId=1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			// create some courses
+			Course course1 = new Course("Air Guitar - The Ultimate Guide");
+			Course course2 = new Course("The Pinball Master Class");
+			
+			// add courses to instructor
+			tempInstructor.add(course1);
+			tempInstructor.add(course2);
+			
+			// save the courses
+			session.save(course1);
+			session.save(course2);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
